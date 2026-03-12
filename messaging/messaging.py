c23293fa614d37f091bd99653521ee38600e73c3
YOUNG HO CHA <ganachoco@linecorp.com>
2026-03-12 19:45:15 +0900

    Update fcm test script
    - Detect Project ID automatically from service setting
    - Clean up legacy logic

--- messaging/messaging.py
+++ messaging/messaging.py
@@ -17,11 +17,9 @@ import google.auth.transport.requests
 
 from google.oauth2 import service_account
 
+# Project Id will be detected from service account key
 PROJECT_ID = '<YOUR-PROJECT-ID>'
 BASE_URL = 'https://fcm.googleapis.com'
-FCM_ENDPOINT = 'v1/projects/' + PROJECT_ID + '/messages:send'
-FCM_URL = BASE_URL + '/' + FCM_ENDPOINT
-FCM_LEGACY_URL = BASE_URL + "/fcm/send"
 SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
 
 # [START retrieve_access_token]
@@ -30,26 +28,26 @@ def _get_access_token():
 
   :return: Access token.
   """
-  credentials = service_account.Credentials.from_service_account_file(
-    'service-account.json', scopes=SCOPES)
+  # https://cloud.google.com/docs/authentication/application-default-credentials?hl=en#order
+  credentials, project = google.auth.default()
+  scoped_credentials = credentials.with_scopes(SCOPES)
   request = google.auth.transport.requests.Request()
-  credentials.refresh(request)
-  return credentials.token
+  scoped_credentials.refresh(request)
+
+  fcm_endpoint = f'v1/projects/{project}/messages:send'
+  fcm_url = BASE_URL + '/' + fcm_endpoint
+
+  return fcm_url, scoped_credentials.token
 # [END retrieve_access_token]
 
-def _send_fcm_message(fcm_message, token):
+def _send_fcm_message(fcm_message):
   """Send HTTP request to FCM with given message.
 
   Args:
     fcm_message: JSON object that will make up the body of the request.
   """
-  global FCM_URL
-  if token:
-    auth = "key=" + token
-    FCM_URL = FCM_LEGACY_URL
-  else:
-    auth = "Bearer " + _get_access_token()
-
+  fcm_url, token =  _get_access_token()
+  auth = "Bearer " + token
 
   # [START use_access_token]
   headers = {
@@ -57,7 +55,7 @@ def _send_fcm_message(fcm_message, token):
     'Content-Type': 'application/json; UTF-8',
   }
   # [END use_access_token]
-  resp = requests.post(FCM_URL, data=json.dumps(fcm_message), headers=headers)
+  resp = requests.post(fcm_url, data=json.dumps(fcm_message), headers=headers)
 
   if resp.status_code == 200:
     print('Message sent to Firebase for delivery, response:')
@@ -84,7 +82,10 @@ def _build_common_message(token, fcmv1):
     return {
       'message': {
         'token': token,
-        'notification': notification
+        'notification': notification,
+        "android":{
+          "priority":"high"
+        }
       }
     }
   else:
@@ -127,21 +128,20 @@ def _build_override_message(token, use_fcmv1):
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--message')
-  parser.add_argument('--use-http')
   parser.add_argument('--token')
   args = parser.parse_args()
-  use_fcmv1 = not bool(args.use_http)
+  use_fcmv1 = True
 
   if args.message and args.message == 'common-message':
     common_message = _build_common_message(args.token, use_fcmv1)
     print('FCM request body for message using common notification object:')
     print(json.dumps(common_message, indent=2))
-    _send_fcm_message(common_message, args.use_http)
+    _send_fcm_message(common_message)
   elif args.message and args.message == 'override-message':
     override_message = _build_override_message(args.token, use_fcmv1)
     print('FCM request body for override message:')
     print(json.dumps(override_message, indent=2))
-    _send_fcm_message(override_message, args.use_http)
+    _send_fcm_message(override_message)
   else:
     print('''Invalid command. Please use one of the following commands:
 python messaging.py --message=common-message
